718981ccfe69980c5bf7a97f6ae6aaaab6da4aff
Sam97139 <lsam97139@gmail.com>
2019-04-26 15:18:05 -0400

    Update index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+   
+    <title>Guadabat</title>
+    
+
+</head>
+
+<body>
+
+    <ul>
+  <li><a href="#acceuil">Acceuil</a></li>
+  <li><a href="#article">Article</a></li>
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Commande</a>
+    <div class="dropdown-content">
+      <a href="#beton">Bétons</a>
+      <a href="#granulat">Granulats</a>
+      <a href="#ciment">Ciments</a>
+    </div>
+  </li>
+  <li><a href="#client">Client</a></li>
+</ul>
+
+</body>
+     
+</html>
