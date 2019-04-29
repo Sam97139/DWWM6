64802ab9ce2052822734176be06dedf33150d56c
Sam97139 <lsam97139@gmail.com>
2019-04-29 13:41:02 -0400

    deleted:    beton.php 	deleted:    formulaire.php

--- beton.php
+++
@@ -1,8 +0,0 @@
-<form name='monform' action='' method='post'>
-<pre>
- <label>Produit</label><input type='text' name='num_produit'>
- <label>Quantité</label><input type='text' name='quantite'>
- <label>Numero Client</label><input type='text' name='num_client'>
-</pre>
-<input type='submit' name='envoyer' value='valider'>
-</form>
