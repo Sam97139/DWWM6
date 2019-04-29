2d060f0af88c03ff12e2018c7f9243fd11a1b1a2
Sam97139 <lsam97139@gmail.com>
2019-04-29 11:17:19 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
+   
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style.css"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -13,7 +14,7 @@
 <body>
     <ul>
   <li><a href="#acceuil">Acceuil</a></li>
-  <li><a href="#article">Article</a></li>
+  <li><a href="article">Article</a></li>
   <li class="drop">
     <a href="#commande" class="commande">Commande</a>
   </li>
